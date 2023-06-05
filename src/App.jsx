8016fce81f2336f3872075528d70d439fe7cc026
HankHsuABoo <hank183753@gmail.com>
2023-06-05 11:08:10 +0800

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage.jsx'
 
-export function App() {
+export default function App() {
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
