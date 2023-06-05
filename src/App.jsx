32a99cd6b5750f2fc1928ffe9d4ffa03eb83face
JUJUCW <juchung424@gmail.com>
2023-06-05 19:18:20 +0800

    FEAT/ add main page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage.jsx'
+import MainPage from 'pages/MainPage';
 
 export default function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
+          <Route path="main" element={<MainPage/>} />
           <Route path="login" element={<LoginPage />} />
         </Routes>
       </BrowserRouter>
