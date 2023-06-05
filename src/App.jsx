80836b10caf76d56815596f9eae23dab37f9c3c5
HankHsuABoo <hank183753@gmail.com>
2023-06-05 21:58:58 +0800

    add AdminLoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage/LoginPage.jsx'
+import LoginPage from './pages/LoginPage/LoginPage.jsx';
 import SignUpPage from './pages/SignUpPage/SignUpPage.jsx';
+import AdminLoginPage from './pages/AdminLoginPage/AdminLoginPage.jsx'
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="login" element={<LoginPage />} />
           <Route path="signup" element={<SignUpPage />} />
+          <Route path="admin/login" element={<AdminLoginPage />} />
         </Routes>
       </BrowserRouter>
     </div>
