6fd6d59c2541f4e4444014867a43def4d2001bcb
JUJUCW <juchung424@gmail.com>
2023-06-05 17:30:39 +0800

    FEAT/ add font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 
+import './index.scss'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
