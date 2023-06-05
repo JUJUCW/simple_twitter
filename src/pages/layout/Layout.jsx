82374b1f54778d639c476c7253777ef03c139beb
JUJUCW <juchung424@gmail.com>
2023-06-05 15:29:19 +0800

    FEAT/ add layout page

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,3 +1,15 @@
-import MainContainer from 'components/MainContainer/MainContainer'
-import NavBarContainer from 'components/NavBarContainer/NavBarContainer'
-import SuggestFollowContainer from 'components/SuggestFollowContainer/SuggestFollowContainer'
+import style from './Layout.module.scss';
+
+import MainContainer from 'components/MainContainer/MainContainer';
+import NavBarContainer from 'components/NavBarContainer/NavBarContainer';
+import SuggestFollowContainer from 'components/SuggestFollowContainer/SuggestFollowContainer';
+
+export default function Layout() {
+    return (
+        <div className={style.mainContainer}>
+            <NavBarContainer />
+            <MainContainer />
+            <SuggestFollowContainer />
+        </div>
+    );
+}
