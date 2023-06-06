57ec63cea0d2af678b77ec944a5156e3c737ccf2
HankHsuABoo <hank183753@gmail.com>
2023-06-06 20:19:16 +0800

    adjest NavBar styles

--- src/components/NavBarContainer/NavBarContainer.jsx
+++ src/components/NavBarContainer/NavBarContainer.jsx
@@ -11,46 +11,39 @@ export default function NavBarContainer({role}) {
     return (
         <>
             <div className={styles.navBarContainer}>
+                <Link to="/homePage">
+                    
+                    <img className={styles.img} src={logo} alt="logo" />
+                  
+                </Link>
                 <div className={styles.main}>
-                    <Link to="/homePage">
-                        <div className={styles.logo}>
-                            <img className={styles.img} src={logo} alt="logo" />
-                        </div>
-                    </Link>
                     {role ==='user' && 
                     <>
-                        <div className={styles.menu}>
-                            <NavLink to="/homePage" className={styles.btnLink}>
-                                <NavItem iconStyle={'iconHomeAction'} altName="home" title="首頁" />
-                            </NavLink>
+                        <NavLink to="/main">
+                            <NavItem iconStyle={'iconHome'} altName="main" title="首頁" />
+                        </NavLink>
 
-                            <NavLink to="/TweetPage" className={styles.btnLink}>
-                                <NavItem iconStyle={'iconUser'} altName="user" title="個人資料" />
-                            </NavLink>
+                        <NavLink to="/TweetPage" >
+                            <NavItem iconStyle={'iconUser'} altName="user" title="個人資料" />
+                        </NavLink>
 
-                            <NavLink to="/SettingPage" className={styles.btnLink}>
-                                <NavItem iconStyle={'iconSetting'} altName="setting" title="設定" />
-                            </NavLink>
-                        </div>
-                        <div className={styles.tweet}>
-
-                            <div className={styles.btnTweet}>
-                                <Button title="推文" size="navTweet" isAction></Button>
-                            </div>
-                        </div>
+                        <NavLink to="/setting" >
+                            <NavItem iconStyle={'iconSetting'} altName="setting" title="設定" />
+                        </NavLink>
+                    
+                        <Button title="推文" size="navTweet" isAction></Button>
+                       
                     </>
                     }
                     {role ==='admin' && 
                     <>
-                        <div className={styles.menu}>
-                            <NavLink to="/homePage" className={styles.btnLink}>
-                                <NavItem iconStyle={'iconHomeAction'} altName="home" title="推文清單" />
-                            </NavLink>
+                        <NavLink to="/admin/tweets" >
+                            <NavItem iconStyle={'iconHome'} altName="home" title="推文清單" />
+                        </NavLink>
 
-                            <NavLink to="/TweetPage" className={styles.btnLink}>
-                                <NavItem iconStyle={'iconUser'} altName="user" title="使用者列表" />
-                            </NavLink>
-                        </div>
+                        <NavLink to="/admin/users" >
+                            <NavItem iconStyle={'iconUser'} altName="user" title="使用者列表" />
+                        </NavLink>
                     </>
                     }
                 </div>
