816fb68d5b692200f5c50722bad1f69ab56caac9
HankHsuABoo <hank183753@gmail.com>
2023-06-06 12:49:26 +0800

    add SettingPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage.jsx';
 import SignUpPage from './pages/SignUpPage/SignUpPage.jsx';
 import AdminLoginPage from './pages/AdminLoginPage/AdminLoginPage.jsx'
+import SettingPage from './pages/SettingPage/SettingPage.jsx'
 import ButtonShowCasePage from './pages/ButtonShowCasePage/ButtonShowCasePage.jsx'
+
 
 export default function App() {
   return (
@@ -13,6 +15,7 @@ export default function App() {
           <Route path="login" element={<LoginPage />} />
           <Route path="signup" element={<SignUpPage />} />
           <Route path="admin/login" element={<AdminLoginPage />} />
+          <Route path="setting" element={<SettingPage />} />
           <Route path="show" element={<ButtonShowCasePage />} />
         </Routes>
       </BrowserRouter>
