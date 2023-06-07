2eb304c4ee60f5edc5e235050c372665bf00b516
HankHsuABoo <hank183753@gmail.com>
2023-06-07 15:37:32 +0800

    adjest mainPage structure

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,6 +3,9 @@ import styles from './MainPage.module.scss';
 import MainContainer from '../../components/MainContainer/MainContainer.jsx';
 import NavBarContainer from '../../components/NavBarContainer/NavBarContainer.jsx';
 import SuggestFollowContainer from '../../components/SuggestFollowContainer/SuggestFollowContainer.jsx';
+import Header from '../../components/Header/Header.jsx'
+import TweetInput from '../../components/MainContainer/TweetInput/TweetInput.jsx';
+import TweetListItem from '../../components/UIComponents/NavItem/ListItem/TweetListItem.jsx';
 
 export default function MainPage() {
     return (
@@ -11,9 +14,13 @@ export default function MainPage() {
             <div className={styles.navBarContainer}>
                 <NavBarContainer role='user'page='main'/>
             </div>
-            <div className={styles.mainContainer}>
-                <MainContainer />
-            </div>
+
+            <MainContainer>
+                <Header title='首頁'/>
+                <TweetInput/>
+                <TweetListItem/>
+            </MainContainer>
+
             <div className={styles.suggestFollowContainer}>
                 <SuggestFollowContainer />
             </div>
