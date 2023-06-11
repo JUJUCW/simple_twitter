b5f9c3fa99d78dbef4ed425c79aeb69821ed835e
HankHsuABoo <hank183753@gmail.com>
2023-06-12 00:25:41 +0800

    feat/connect admin get all users api

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,5 +1,6 @@
 import { apiHelper } from '../utility/helper.js'
 
+// get admin all tweets 
 export const adminGetAllTweets = async () => {
   try{
     const {data} = await apiHelper.get(`/admin/tweets`)
@@ -10,3 +11,16 @@ export const adminGetAllTweets = async () => {
     return error
   }
 } 
+
+// get admin all users
+
+export const adminGetAllUsers = async() => {
+  try{
+    const{data} = await apiHelper.get(`/admin/users`)
+    console.log(data)
+    return data
+  } catch (error) {
+    console.log('[Get user tweets Failed]:', error)
+    return error
+  }
+}
