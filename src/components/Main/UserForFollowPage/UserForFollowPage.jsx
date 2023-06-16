3bc51feb1af8347fbe1bef48a0991bb7a461848e
HankHsuABoo <hank183753@gmail.com>
2023-06-16 21:51:56 +0800

    add followpage RWD styles

---
+++ src/components/Main/UserForFollowPage/UserForFollowPage.jsx
@@ -0,0 +1,136 @@
+import { Link, useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import Button from '../../Button/Button.jsx';
+import Header from "../../../components/Header/Header.jsx"
+import UserEditModal from '../../Modal/UserEditModal/UserEditModal.jsx';
+import msg from '../../../assets/icons/user/user_msg.png';
+import notify from '../../../assets/icons/user/user_notfi.png';
+import { useAuth } from '../../../context/AuthContext.jsx'
+import { getUser } from '../../../api/user.js'
+import { followUser, unFollowUser } from '../../../api/followship.js';
+import { useDataStatus } from '../../../context/DataContext.jsx'
+import styles from './UserForFollowPage.module.scss';
+
+export default function UserForFollowPage () {
+    const [userProfile, setUserProfile] = useState('');
+    const URL = useParams();
+    const userId = userProfile?.id;
+    const isFollowed = userProfile?.isFollowed
+    const { currentUser } = useAuth();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isClicked, setIsClicked] = useState(isFollowed);
+    const {isDataUpdate, setIsDataUpdate } = useDataStatus();
+    
+    
+    
+    useEffect(() => {
+        const getUserInfo = async () => {
+            try {
+                const data = await getUser(URL.UserId);
+                if (data.status === 'error') {
+                    console.log(data.message);
+                    return;
+                }
+                if (data) {
+                    // update data
+                    await setUserProfile(data);
+                    await setIsClicked (data.isFollowed)
+                    // console.log(data);
+                }
+            } catch (error) {
+                console.log('獲取使用者資料失敗', error);
+            }
+        };
+        getUserInfo();
+    }, [URL.UserId, isDataUpdate]);
+
+
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleClick = async () => {
+        try {
+            if (isClicked === false) {
+                const data = await followUser(userId);
+                if (data.followingId) {
+                    await setIsClicked(true);
+                    await setIsDataUpdate(!isDataUpdate)
+                }
+            }
+            if (isClicked === true) {
+                const data = await unFollowUser(userId);
+                if (data.followingId) {
+                    await setIsClicked(false);
+                    await setIsDataUpdate(!isDataUpdate)
+                }
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <Header title={userProfile?.name} arrow tweetCount />
+            <div className={styles.userCard}>
+                <div className={styles.cover}>
+                    <img src={userProfile?.coverPhoto} alt="cover" className={styles.bgImg} />
+                </div>
+                <div className={styles.userInfoAvatar}>
+                    <img src={userProfile?.avatar} alt="avatar" className={styles.img} />
+                </div>
+                {userId !== currentUser?.id ? (
+                <div className={styles.btnContainer}>
+                    <div className={styles.msgIcon}>
+                        <img src={msg} alt="msg" className={styles.msgImg} />
+                    </div>
+                    <div className={styles.notifyIcon}>
+                        <img src={notify} alt="" className={styles.notifyImg} />
+                    </div>
+                    <div onClick={handleClick}>
+                        {isClicked ? (
+                            <Button title="正在跟隨" size="middle" isAction />
+                        ) : (
+                            <Button title="跟隨" size="small" />
+                        )}
+                        
+                    </div>
+                </div>
+                ) : (
+                    <div className={styles.btnContainer}>
+                        <Button title="編輯個人資料" size="edit" onClick={handleOpenModal} />
+                    </div>
+                )}
+                <div className={styles.userInfoCard}>
+                    <div className={styles.userInfoName}>{userProfile?.name}</div>
+                    <div className={styles.userInfoAccount}>@{userProfile?.account}</div>
+                </div>
+
+                <div className={styles.userDescription}>
+                    <div className={styles.descriptionContext}>
+                        {userProfile?.introduction}
+                    </div>
+                    <div className={styles.follows}>
+                        <Link className={styles.routeLink} to={`/user/${userId}/following`}>
+                            <div className={styles.followsFollower}>
+                                <span className={styles.followsCount}>{userProfile?.followingCount||0}位</span>
+                                <span className={styles.followsType}>跟隨中</span>
+                            </div>
+                        </Link>
+                        <Link className={styles.routeLink} to={`/user/${userId}/follower`}>
+                            <div className={styles.followingFollower}>
+                                <span className={styles.followingCount}>{userProfile?.followerCount||0}位</span>
+                                <span className={styles.followingType}>跟隨者</span>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            {isModalOpen && <UserEditModal handleCloseModal={handleCloseModal} id={userId} oriName={userProfile?.name} oriIntroduction={userProfile?.introduction} oriAvatar={userProfile?.avatar} oriCoverImg={userProfile?.coverPhoto}/>}
+        </div>
+    );
+}
--- src/pages/UserFollowerPage/UserFollowerPage.jsx
+++ src/pages/UserFollowerPage/UserFollowerPage.jsx
@@ -1,13 +1,13 @@
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import Header from '../../components/Header/Header.jsx';
+import UserForFollowPage from '../../components/Main/UserForFollowPage/UserForFollowPage.jsx'
 import styles from './UserFollowerPage.module.scss';
 import NavBarContainer from '../../components/Navbar/NavBarContainer/NavBarContainer.jsx';
 import MainContainer from '../../components/Main/MainContainer/MainContainer.jsx';
 import UserToggleMenu from '../../components/Main/UserToggleMenu/UserToggleMenu.jsx';
 import SuggestUserContainer from '../../components/SuggestUser/SuggestUserContainer/SuggestUserContainer.jsx';
 import FollowTypeCard from '../../components/Main/FollowTypeCard/FollowTypeCard.jsx';
-import {getUserFollowers, getUser} from '../../api/user.js'
+import { getUserFollowers } from '../../api/user.js'
 import { useAuth } from '../../context/AuthContext.jsx'
 import { useDataStatus } from '../../context/DataContext.jsx'
 
@@ -17,7 +17,7 @@ export default function UserFollowerPage() {
     const { isAuthenticated, isAuthChecked } = useAuth();
     const navigate = useNavigate();
     const { isDataUpdate } = useDataStatus();
-    const [userProfile, setUserProfile] = useState('')
+  
 
     useEffect(() => {
     const getUserFollower = async () => {
@@ -36,26 +36,6 @@ export default function UserFollowerPage() {
         }
         }
         getUserFollower();
-    }, [URL.UserId, isDataUpdate]);
-
-    useEffect(() => {
-        const getUserInfo = async () => {
-            try {
-                const data = await getUser(URL.UserId);
-                if (data.status === 'error') {
-                    console.log(data.message);
-                    return;
-                }
-                if (data) {
-                    // update data
-                    setUserProfile(data);
-                    // console.log(data);
-                }
-            } catch (error) {
-                console.log('獲取使用者資料失敗', error);
-            }
-        };
-        getUserInfo();
     }, [URL.UserId, isDataUpdate]);
 
     const followerList = users.map((user) => {
@@ -82,14 +62,16 @@ export default function UserFollowerPage() {
         <div className={styles.container}>
             <NavBarContainer role="user" page="main" />       
                 <MainContainer>
-                    <Header title={userProfile.name} arrow tweetCount="25" />
+                    <UserForFollowPage/>
                     <div className={styles.userToggleMenu}>
                         <UserToggleMenu linkName="追隨者" isActive />
                         <Link to={`/user/${URL.UserId}/following`}>
                           <UserToggleMenu linkName="正在追蹤" />
                         </Link>
                     </div>
-                    {followerList}
+                    <div className={styles.navBarContainer}>
+                        {followerList}
+                    </div>
                 </MainContainer>    
             <SuggestUserContainer />
         </div>
