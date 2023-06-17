739ba65a55934bcd3bd8a84f80960e904ff28b6c
JUJUCW <juchung424@gmail.com>
2023-06-17 14:33:18 +0800

    FEAT/ admin page key enter

--- src/pages/AdminLoginPage/AdminLoginPage.jsx
+++ src/pages/AdminLoginPage/AdminLoginPage.jsx
@@ -1,21 +1,21 @@
-import { useState, useEffect, useMemo } from 'react';
-import { Link, useNavigate } from 'react-router-dom'
-import { Toast} from '../../utility/helper.js'
+import { useState, useEffect, useMemo, useCallback } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Toast } from '../../utility/helper.js';
 
-import AuthInput from '../../components/Auth/AuthInput/AuthInput.jsx'
-import AuthPageContainer from '../../components/Auth/AuthPageContainer/AuthPageContainer.jsx'
-import Button from '../../components/Button/Button.jsx'
+import AuthInput from '../../components/Auth/AuthInput/AuthInput.jsx';
+import AuthPageContainer from '../../components/Auth/AuthPageContainer/AuthPageContainer.jsx';
+import Button from '../../components/Button/Button.jsx';
 
-import { useAuth } from '../../context/AuthContext.jsx'
-import styles from './AdminLoginPage.module.scss'
+import { useAuth } from '../../context/AuthContext.jsx';
+import styles from './AdminLoginPage.module.scss';
 
-export default function AdminLoginPage () {
-  const [account, setAccount] = useState("");
-  const [password, setPassword] = useState("");
-  const { login, isAuthenticated } = useAuth();
-  const navigate = useNavigate();
+export default function AdminLoginPage() {
+    const [account, setAccount] = useState('');
+    const [password, setPassword] = useState('');
+    const { login, isAuthenticated } = useAuth();
+    const navigate = useNavigate();
 
-  const isValid = useMemo(() => {
+    const isValid = useMemo(() => {
         if (!account || account.length > 50) {
             return false;
         }
@@ -25,57 +25,79 @@ export default function AdminLoginPage () {
         return true;
     }, [account, password]);
 
-  const handleClick = async () => {
+    const handleClick = useCallback(async () => {
+        if (!isValid) {
+            Toast.fire({
+                title: '請確認帳號密碼!',
+                icon: 'error',
+            });
+            return;
+        }
 
-    if (!isValid) {
+        const success = await login({
+            account,
+            password,
+            role: 'admin',
+        });
+        // login success
+        if (success) {
+            Toast.fire({
+                title: '登入成功',
+                icon: 'success',
+            });
+            return;
+            // login fail
+        }
         Toast.fire({
-            title: '請確認帳號密碼!',
+            title: '帳號不存在',
             icon: 'error',
         });
-        return;
-    }
-    
-    const success = await login({
-      account,
-      password,
-      role:"admin"
-    });
-    // login success
-    if (success) {
-      Toast.fire({
-        title: "登入成功",
-        icon: "success",
-      });
-      return;
-    // login fail
-    } 
-    Toast.fire({
-        title: "帳號不存在",
-        icon: "error",
-      });
-    await setPassword("")
-  }
+        await setPassword('');
+    }, [account, password, login, isValid]);
 
-  useEffect(() => {
-      if (isAuthenticated) {
-        navigate('/admin/tweets');
-      }
-    }, [navigate, isAuthenticated]);
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate('/admin/tweets');
+        }
+        const keyDownHandler = (event) => {
+            // console.log('User pressed: ', event.key);
 
-  return (
-    <AuthPageContainer title='後台登入'>
-      <AuthInput label='帳號' value={account} placeholder='請輸入帳號' onChange={(accountInputValue) => setAccount(accountInputValue)}
-      notification='字數超出上限!' wordsLimit={50}
-      />
-      <AuthInput label='密碼' type='password' value={password} placeholder='請輸入密碼' onChange={(passwordInputValue) => setPassword(passwordInputValue)}
-      notification='字數超出上限!' wordsLimit={50}
-      />
-      <Button title='登入' size='large' isAction onClick={handleClick}></Button>
-      <div className={styles.link}>
-        <Link to="/login">
-          <span className={styles.span}>前台登入</span>
-        </Link>
-      </div>
-    </AuthPageContainer>
-  );
+            if (event.key === 'Enter') {
+                event.preventDefault();
+                handleClick();
+            }
+        };
+        document.addEventListener('keydown', keyDownHandler);
+        return () => {
+            document.removeEventListener('keydown', keyDownHandler);
+        };
+    }, [handleClick, navigate, isAuthenticated]);
+
+    return (
+        <AuthPageContainer title="後台登入">
+            <AuthInput
+                label="帳號"
+                value={account}
+                placeholder="請輸入帳號"
+                onChange={(accountInputValue) => setAccount(accountInputValue)}
+                notification="字數超出上限!"
+                wordsLimit={50}
+            />
+            <AuthInput
+                label="密碼"
+                type="password"
+                value={password}
+                placeholder="請輸入密碼"
+                onChange={(passwordInputValue) => setPassword(passwordInputValue)}
+                notification="字數超出上限!"
+                wordsLimit={50}
+            />
+            <Button title="登入" size="large" isAction onClick={handleClick}></Button>
+            <div className={styles.link}>
+                <Link to="/login">
+                    <span className={styles.span}>前台登入</span>
+                </Link>
+            </div>
+        </AuthPageContainer>
+    );
 }
